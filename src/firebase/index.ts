3a88078af5e9d3953d7a1cff0d486cd49e868cd0
karolis527 <karolis527@gmail.com>
2026-02-08 16:59:29 +0200

    Fix: final export and auth check

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -32,10 +32,10 @@ export function initializeFirebase(): FirebaseServices {
 }
 
 // Eksportuojame viską iš kitų failų, kad Assign-Admin ir kiti juos rastų
-export * from './provider';
 export * from './client-provider';
 export * from './auth/use-user';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './errors';
 export * from './error-emitter';
+export * from './provider';
